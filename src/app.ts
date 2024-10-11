79be104aa1cd4c807fb31df83da8a54ce3f23205
FelipeBriosso <fbriosso@hotmail.com>
2024-10-11 09:07:25 -0300

    first commit init

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.REGISTER_PORT;
+
+export const main = async () => {
+  try {
+    app.use(express.json());
+
+    app.use(express.raw({ type: "application/octet-stream", limit: "10mb" }));
+
+    app.use(userRoutes);
+    app.listen(port, async () => {
+    console.log(`Server running on http://localhost:${port}`);
+    });
+  } catch (error: any) {
+    console.log(error.message);
+    }
+}
+
